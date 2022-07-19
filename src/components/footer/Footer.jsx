adfd068936d071a090c7849b78c1a8da6df51c58
Lautaro Asat <lautaronorielasat@gmail.com>
2022-07-19 11:26:40 -0300

    Fix style footer margin top

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useContext } from "react";
+import darkContext from "../../context/darkContext";
 
 import Contact from "./Contact";
 import MobileNav from "./MobileNav";
@@ -6,7 +8,7 @@ import SocialNav from "./SocialNav";
 
 const Footer = () => {
   return (
-    <footer className="bg-[#0f172a] mt-8 p-4">
+    <footer className="bg-[#0f172a] p-4">
       <MobileNav />
       <SocialNav />
       <Contact />
