483d7bb5aabb51301e454baedffe033518723609
Lautaro Asat <lautaronorielasat@gmail.com>
2022-07-30 23:02:51 -0300

    Refactor carrousel

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -4,38 +4,32 @@ import { Link } from "react-router-dom";
 
 const Carrousel = ({ images, className }) => {
   const [currentPosition, setCurrentPosition] = useState(0);
-  const [currentImage, setCurrentImage] = useState({});
+  const [currentImage, setCurrentImage] = useState(images[currentPosition]);
   const { image, alt, description, title } = currentImage;
   const limit = images.length - 1;
-
-  useEffect(() => {
-    setCurrentPosition(0);
-    setCurrentImage(images[currentPosition]);
-  }, []);
 
   useEffect(() => {
     setCurrentImage(images[currentPosition]);
 
     const follower = setInterval(() => {
-      handleCurrentPosition("next");
-    }, 5000);
+      nextImage();
+    }, 8000);
 
     return () => {
       clearInterval(follower);
     };
   }, [currentPosition]);
 
-  const handleCurrentPosition = (mode) => {
-    if (!mode) return;
-    if (mode === "prev") {
-      currentPosition - 1 < 0
-        ? setCurrentPosition(limit)
-        : setCurrentPosition(currentPosition - 1);
-    } else {
-      currentPosition + 1 > limit
-        ? setCurrentPosition(0)
-        : setCurrentPosition(currentPosition + 1);
-    }
+  const nextImage = () => {
+    currentPosition + 1 > limit
+      ? setCurrentPosition(0)
+      : setCurrentPosition(currentPosition + 1);
+  };
+
+  const prevImage = () => {
+    currentPosition - 1 < 0
+      ? setCurrentPosition(limit)
+      : setCurrentPosition(currentPosition - 1);
   };
 
   return (
@@ -45,13 +39,13 @@ const Carrousel = ({ images, className }) => {
         <nav className="w-full px-1 flex justify-between absolute top-[88px]">
           <button
             className="h-8 w-8 rounded-full bg-[#0f172a93] text-center grid place-items-center"
-            onClick={() => handleCurrentPosition("prev")}
+            onClick={prevImage}
           >
             <BiLeftArrow className="w-6 h-6 text-secondary-color" />
           </button>
           <button
             className="h-8 w-8 rounded-full bg-op-primary-color imary-color text-center grid place-items-center"
-            onClick={() => handleCurrentPosition("next")}
+            onClick={nextImage}
           >
             <BiRightArrow className="w-6 h-6 text-secondary-color" />
           </button>
