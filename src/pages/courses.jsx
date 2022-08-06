a078dbd37f70d3c9869ec81471a77f97bbd73303
Lautaro Asat <lautaronorielasat@gmail.com>
2022-08-06 20:28:02 -0300

    Create Cards in courses

--- src/pages/courses.jsx
+++ src/pages/courses.jsx
@@ -1,19 +1,45 @@
 import React from "react";
 import Title from "../components/Title";
-const cursos = [1, 2, 3, 4, 5, 6, 7];
+import Paragraph from "../components/Paragraph";
+import Main from "../components/Main";
+import { Link } from "react-router-dom";
+import { useStyles } from "../hooks/useStyles";
+
+const COURSES = [
+  { year: "1er año", index: 1 },
+  { year: "2do año", index: 2 },
+  { year: "3er año", index: 3 },
+  { year: "4to año", index: 4 },
+  { year: "5to año", index: 5 },
+  { year: "6to año", index: 6 },
+  { year: "7mo año", index: 7 },
+];
 
 const Courses = () => {
+  const { borderStyles, buttonStyles } = useStyles();
   return (
-    <>
-      <Title>Cursos</Title>
-      {cursos.map((el) => {
+    <Main>
+      <Title
+        className={`text-center mb-10 pb-2 border-b-2 border-dotted ${borderStyles}`}
+      >
+        Cursos
+      </Title>
+      <Paragraph>Vea los contenidos y objetivos de cada curso</Paragraph>
+      {COURSES.map((course) => {
         return (
-          <div key={el} className="bg-blue-300 m-4 p-4 rounded-2xl">
-            <h2 className="text-left text-2xl">Curso numero {el}</h2>
-          </div>
+          <Link to={`${course.index}`}>
+            <div
+              key={course.index}
+              className={` m-4 p-4 rounded-2xl border-b overflow-hidden ${buttonStyles}`}
+            >
+              <Paragraph className="rounded-2xl text-left ">
+                {course.year}
+              </Paragraph>
+            </div>
+          </Link>
         );
       })}
-    </>
+    </Main>
   );
 };
 
